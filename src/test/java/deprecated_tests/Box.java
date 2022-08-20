1edee5f554f9242480930d5e605717314c9d37ee
yaodong <yaodong.sun@outlook.com>
2022-08-20 16:37:33 +0200

    重构了一下项目结构

---
+++ src/test/java/deprecated_tests/Box.java
@@ -0,0 +1,15 @@
+package src.test.java.deprecated_tests;
+
+public class Box<kurse> {
+
+        private kurse t;
+
+        public void set(kurse t) {
+            this.t = t;
+        }
+
+        public kurse get() {
+            return t;
+        }
+
+}
---
+++ src/test/java/deprecated_tests/Exercise4.java
@@ -0,0 +1,19 @@
+package src.test.java.deprecated_tests;
+
+/**
+ * 尝试
+ */
+public class Exercise4 {
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<>();
+        Box<String> stringBox = new Box<>();
+
+        integerBox.set(10);
+        stringBox.set("菜鸟教程");
+
+
+        System.out.printf("整型值为 :%d\n\n", integerBox.get());
+        System.out.printf("字符串为 :%s\n", stringBox.get());
+
+    }
+}
